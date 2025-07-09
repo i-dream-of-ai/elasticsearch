862dc9ccebbc00b65ab9fd9ad91d5446a6b4b0b8
Sylvain Wallez <sylvain@elastic.co>
2025-07-09 16:50:48 +0200

    Port buildkite pipelines from the main branch

---
+++ .buildkite/docker-push.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+version_tag=$(awk -F ' = ' '$1 ~ /version/ { gsub(/[\"]/, "", $2); printf("%s",$2) }' Cargo.toml)
+elastic_image="docker.elastic.co/mcp/elasticsearch"
+
+# build image
+docker buildx create --use --name builder
+docker buildx inspect --bootstrap
+docker buildx build -t "$elastic_image:$version_tag" --platform linux/amd64,linux/arm64 --builder builder --load .
+docker tag "$elastic_image:$version_tag" "$elastic_image:latest"
+
+# push to docker.elastic.co
+ELASTIC_PASSWORD=$(vault read -field=password secret/ci/elastic-mcp-server-elasticsearch/devtoolsmachine)
+docker login -u "devtoolsmachine" -p "$ELASTIC_PASSWORD" docker.elastic.co
+docker push "$elastic_image:$version_tag"
+docker push "$elastic_image:latest"
+docker logout docker.elastic.co
